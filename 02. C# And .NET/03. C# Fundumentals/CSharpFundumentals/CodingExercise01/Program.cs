230ad156ebd1c72a67a03d267f7081e2f31de8b4
oroumand <ar.oroumand@gmail.com>
2024-01-18 09:28:27 +0330

    Coding Exercise 01 Coding Exercise 02

---
+++ 02. C# And .NET/03. C# Fundumentals/CSharpFundumentals/CodingExercise01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Coding Exercise 01");
+
+var num01 = 10;
+var num02 = 20;
+var num03 = 30;
+
+var num04 = (num01 * num02) / num03;
+
+Console.WriteLine(num04);
+Console.ReadLine();
--- 02. C# And .NET/03. C# Fundumentals/CSharpFundumentals/ToDoApp/Program.cs
+++ 02. C# And .NET/03. C# Fundumentals/CSharpFundumentals/ToDoApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("[S]ee All Todo");
+Console.WriteLine("[A]dd a todo");
+Console.WriteLine("[R]emove a todo");
+Console.WriteLine("[E]xit todo app");
+
+var num01 = 10;
+var num02 = 20;
+
+// == , !=, >, >=, <, <=
+//!, &&, ||
+bool IsTrue1 = true && true;
+bool IsTrue2 = true && false;
+bool IsTrue3 = false && true;
+bool IsTrue4= false && false;
+bool IsTrue5= true && false;
+
+bool IsLessThan = 10 < 20;
+
+
+Console.WriteLine(IsTrue1);
+Console.WriteLine(IsTrue2);
+Console.WriteLine(IsTrue3);
+Console.WriteLine(IsTrue4);
+
+
+Console.ReadLine();
