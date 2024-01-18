1161c53fdc4acd4d2dbc750c74ff77a8483bf816
oroumand <ar.oroumand@gmail.com>
2024-01-18 09:40:13 +0330

    CodingExercise02

--- 02. C# And .NET/03. C# Fundumentals/CSharpFundumentals/CodingExercise02/Program.cs
+++ 02. C# And .NET/03. C# Fundumentals/CSharpFundumentals/CodingExercise02/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var AliAge = 40;
+var HosseinAge = 30;
+var IsAliGrater = AliAge > HosseinAge;
+var IsHosseinGrater  = AliAge < HosseinAge;
+var AreTheyInTheSameAge = AliAge ==  HosseinAge;
+
+Console.WriteLine("Ali is grater than Hossein: "+ IsAliGrater);
+Console.WriteLine("Hossin is grater than Ali: "+ IsHosseinGrater);
+Console.WriteLine("They are in the same age: "+ AreTheyInTheSameAge);
+Console.ReadLine();
