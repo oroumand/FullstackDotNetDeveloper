1fc964371087b7a25a7c981a7b0ee357261c3a91
oroumand <ar.oroumand@gmail.com>
2024-02-11 10:36:56 +0330

    feat: add AddtoDo and Exit

--- 02. C# And .NET/03. C# Fundumentals/CSharpFundumentals/ToDoApp/Program.cs
+++ 02. C# And .NET/03. C# Fundumentals/CSharpFundumentals/ToDoApp/Program.cs
@@ -1,39 +1,64 @@
 ﻿
-Console.WriteLine("[S]ee All Todos");
-Console.WriteLine("[A]dd a todo");
-Console.WriteLine("[R]emove a todo");
-Console.WriteLine("[E]xit todo app");
-char userInput = Console.ReadKey().KeyChar;
 
-switch (userInput)
+Console.WriteLine("Hello. Wellcome to Todo App. Pleas select an option.");
+bool isExitSelected = false;
+List<string> Todos = [];
+
+while (!isExitSelected)
 {
-    case 'S':
-    case 's':
-        PrintMessage("See All todos");
-        break;
-    case 'A':
-    case 'a':
-        PrintMessage("Add a todo");
-        break;
-    case 'R':
-    case 'r':
-        PrintMessage("Remove a todo");
-        break;
-    case 'E':
-    case 'e':
-        PrintMessage("Exit todo app");
-        break;
-    default:
-        PrintMessage("Invalid character please try ageain!");
-        break;
+    Console.WriteLine();
+    Console.WriteLine("[S]ee all todos.");
+    Console.WriteLine("[A]dd a todo.");
+    Console.WriteLine("[R]emove a todo.");
+    Console.WriteLine("[E]xit");
+
+    string userInput = Console.ReadLine();
+    switch (userInput)
+    {
+        case "S":
+        case "s":
+            Console.WriteLine("See all todos");
+            break;
+        case "A":
+        case "a":
+            AddTodo();
+            break;
+        case "R":
+        case "r":
+            Console.WriteLine("Remove a todo");
+            break;
+        case "E":
+        case "e":
+            isExitSelected = true;
+            break;
+        default:
+            Console.WriteLine("Invalid input.Please select a correct option.");
+            break;
+    }
 }
 
 
-Console.ReadLine();
 
-void PrintMessage(string userInputMessage)
+void AddTodo()
 {
-    Console.WriteLine("User Select: " + userInputMessage);
+    bool isValidMessage = false;
+    while (!isValidMessage)
+    {
+        Console.WriteLine();
+        Console.Write("Plese enter the todo message:");
+        string inputMessage = Console.ReadLine();
+        if (inputMessage == "")
+        {
+            Console.WriteLine("Invali message: message can not be empty.");
+        }
+        else if (Todos.Contains(inputMessage))
+        {
+            Console.WriteLine("Invali message: message can not be duplicate.");
+        }
+        else
+        {
+            isValidMessage = true;
+            Todos.Add(inputMessage);
+        }
+    }
 }
-
-
