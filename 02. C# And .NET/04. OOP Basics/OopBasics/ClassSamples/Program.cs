b231c791d6694d987bdbf25d848cf451e680f74c
oroumand <ar.oroumand@gmail.com>
2024-03-07 09:04:02 +0330

    ClassSamples

---
+++ 02. C# And .NET/04. OOP Basics/OopBasics/ClassSamples/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Rectangle rectangle = new(10, 20);
+rectangle.Print();
+
+
+Console.ReadLine();
+class Rectangle
+{
+    private int _width = 3;
+    private int _height = 5;
+    public Rectangle(int width, int height)
+    {
+        _width = width;
+        _height = height;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Width:{_width}");
+        Console.WriteLine($"Width:{_height}");
+    }
+}
