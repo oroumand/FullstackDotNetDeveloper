ef6b582238d730945c4942c68577530641f34931
oroumand <ar.oroumand@gmail.com>
2024-03-07 07:11:34 +0330

    04. OOP Basics

---
+++ 02. C# And .NET/04. OOP Basics/OopBasics/OopBasics/Program.cs
@@ -0,0 +1,14 @@
+﻿
+DateTime dateTime = DateTime.Now;
+Console.WriteLine(dateTime.Year);
+Console.WriteLine(dateTime.Month);
+
+Console.WriteLine(dateTime.Day);
+
+Console.WriteLine(dateTime.Hour);
+Console.WriteLine(dateTime.Minute);
+Console.WriteLine(dateTime.Second);
+Console.WriteLine(dateTime.Millisecond);
+Console.WriteLine(dateTime.ToLongDateString());
+Console.WriteLine(dateTime.ToShortDateString());
+Console.Read();
