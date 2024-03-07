3a6e47c92257ce62af76aa6cb0b2a1b306a0b800
oroumand <ar.oroumand@gmail.com>
2024-03-07 17:54:58 +0330

    SRP Before

---
+++ 02. C# And .NET/04. OOP Basics/OopBasics/SRP/Products.cs
@@ -0,0 +1,42 @@
+﻿namespace SRP;
+public class Products
+{
+    private List<string> products = new();
+
+    public void AddProduct(string productName)
+    {
+        if (IsValidProductName(productName))
+        {
+            products.Add(productName);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Product Name");
+        }
+    }
+
+    private bool IsValidProductName(string productName) =>
+       !(string.IsNullOrEmpty(productName)) && productName.Length >= 2 && productName.Length <= 100;
+
+    public void Save()
+    {
+        File.WriteAllLines(GetFileName(), products);
+    }
+
+    public void Load()
+    {
+        var lines = File.ReadAllLines(GetFileName());
+        foreach (var line in lines)
+        {
+            AddProduct(line);
+        }
+    }
+
+    public void PrintProductNames()
+    {
+        string productFormated = string.Join(", ", products);
+        Console.WriteLine(productFormated);
+    }
+
+    public string GetFileName() => "productNames.txt";
+}
---
+++ 02. C# And .NET/04. OOP Basics/OopBasics/SRP/Program.cs
@@ -0,0 +1,26 @@
+﻿using SRP;
+
+Products products = new();
+
+bool fileExists = File.Exists(products.GetFileName());
+
+if (!fileExists)
+{
+    Console.Write("There is no file. Get the number of products: ");
+    int productCount = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < productCount; i++)
+    {
+        Console.Write($"Please get name of {i + 1} product:");
+        products.AddProduct(Console.ReadLine());
+    }
+    products.Save();
+}
+else
+{
+    products.Load();
+    products.PrintProductNames();
+}
+
+Console.WriteLine("Finished!");
+Console.ReadLine();
