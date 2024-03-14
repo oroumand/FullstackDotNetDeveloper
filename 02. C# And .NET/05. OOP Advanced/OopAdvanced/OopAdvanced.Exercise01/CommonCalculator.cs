abd0e8b4939fa8392f10fe248ecf4e5e8df2350e
oroumand <ar.oroumand@gmail.com>
2024-03-14 16:20:53 +0330

    OopAdvanced.Exercise01

---
+++ 02. C# And .NET/05. OOP Advanced/OopAdvanced/OopAdvanced.Exercise01/CommonCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopAdvanced.Exercise01
+{
+    public class CommonCalculator
+    {        
+        public int Calculate(List<int> ints)
+        {
+            var result = 0;
+            foreach (var i in ints)
+            {
+                if (CehckNeedAdd(i))
+                {
+                    result += i;
+
+                }
+            }
+            return result;
+        }
+
+        protected virtual bool CehckNeedAdd(int i)
+        {
+            return true;
+        }
+    }
+
+    public class EvenCalculator : CommonCalculator
+    {
+        protected override bool CehckNeedAdd(int i)
+        {
+            return i > 0;
+        }
+    }
+
+    public class OddCalculator : CommonCalculator
+    {
+        protected override bool CehckNeedAdd(int i)
+        {
+            return i < 0;
+        }
+    }
+}
---
+++ 02. C# And .NET/05. OOP Advanced/OopAdvanced/OopAdvanced.Exercise01/Program.cs
@@ -0,0 +1,14 @@
+﻿using OopAdvanced.Exercise01;
+
+List<int> numbers = [1, 10, 20, 34, -10, -55, 123];
+CommonCalculator commonCalculator = new CommonCalculator();
+EvenCalculator evenCalculator = new EvenCalculator();
+OddCalculator oddCalculator = new OddCalculator();
+int commonResulst= commonCalculator.Calculate(numbers);
+int evenResult = evenCalculator.Calculate(numbers);
+int oddResult = oddCalculator.Calculate(numbers);
+
+Console.WriteLine(commonResulst);
+Console.WriteLine(evenResult);
+Console.WriteLine(oddResult);
+Console.ReadLine();
