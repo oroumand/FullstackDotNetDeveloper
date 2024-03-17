fff4c6bde89ef2046682ac2ef5056d6d3aa03e41
oroumand <ar.oroumand@gmail.com>
2024-03-17 20:52:58 +0330

    OopAdvanced.AbstractSamples

---
+++ 02. C# And .NET/05. OOP Advanced/OopAdvanced/OopAdvanced.AbstractSamples/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Animal a = new Animal();
+Cat cat = new Cat();
+Animal dog = new Dog();
+
+public abstract class Animal
+{
+    public string Name { get; set; }
+
+    public abstract  string SaySomthing();
+}
+
+public abstract class Mammal : Animal
+{
+}
+
+public class Cat : Mammal
+{
+    public override string SaySomthing()
+    {
+        return "Mio";
+    }
+
+}
+public class Dog : Mammal
+{
+    public override string SaySomthing()
+    {
+        return "Hap";
+    }
+}
