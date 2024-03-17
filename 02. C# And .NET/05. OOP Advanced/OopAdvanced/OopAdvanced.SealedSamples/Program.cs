bdc13cce3ea26ca45958850dec7dbc87d8538b88
oroumand <ar.oroumand@gmail.com>
2024-03-17 21:01:20 +0330

    OopAdvanced.SealedSamples

---
+++ 02. C# And .NET/05. OOP Advanced/OopAdvanced/OopAdvanced.SealedSamples/Program.cs
@@ -0,0 +1,32 @@
+﻿public abstract class Animal
+{
+    public string Name { get; set; }
+
+    public abstract string SaySomthing();
+}
+
+public abstract class Mammal : Animal
+{
+}
+
+public class Cat : Mammal
+{
+    public override string SaySomthing()
+    {
+        return "Mio";
+    }
+
+}
+public class Dog : Mammal
+{
+    public override sealed string SaySomthing()
+    {
+        return "Hap";
+    }
+}
+
+public sealed class GermanShepherd:Dog
+{
+
+}
+
