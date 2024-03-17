00a68314c7c5cc51a1e64703791b3b9ddd5f66b0
oroumand <ar.oroumand@gmail.com>
2024-03-17 19:35:24 +0330

    OopAdvanced.BaseSamples

---
+++ 02. C# And .NET/05. OOP Advanced/OopAdvanced/OopAdvanced.BaseSamples/Program.cs
@@ -0,0 +1,5 @@
+﻿using OopAdvanced.BaseSamples.TestClasses;
+
+Student student = new Student("Alireza","Oroumand","123123123");
+Console.WriteLine(student.ToString());
+Console.ReadLine();
---
+++ 02. C# And .NET/05. OOP Advanced/OopAdvanced/OopAdvanced.BaseSamples/TestClasses/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopAdvanced.BaseSamples.TestClasses
+{
+    public class Person
+    {
+        public Person(string firstName,string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+            Console.WriteLine($"Person Constructor {FirstName} {LastName}");
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+
+    public class Student : Person
+    {
+        public Student(string firstName,string lastName,string studentNumber):base(firstName,lastName)
+        {
+            StudentNumber = studentNumber;
+
+            Console.WriteLine($"Student Constructor {FirstName} {LastName}");
+
+        }
+        public string StudentNumber { get; set; }
+        public override string ToString()
+        {
+            return $"{base.ToString()} {StudentNumber}";
+        }
+    }
+
+    public class Temp
+    {
+
+    }
+}
