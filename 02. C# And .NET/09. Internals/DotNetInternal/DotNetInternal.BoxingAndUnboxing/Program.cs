bd590093cf6004663610acfc19c70796b333773d
oroumand <ar.oroumand@gmail.com>
2024-06-12 13:30:19 +0330

    DotNetInternal.BoxingAndUnboxing

---
+++ 02. C# And .NET/09. Internals/DotNetInternal/DotNetInternal.BoxingAndUnboxing/Program.cs
@@ -0,0 +1,10 @@
+﻿int age = 50;
+object ageObj = age;
+
+short intAge = (short)ageObj;
+Console.WriteLine(intAge);
+//foreach (var item in list)
+//{
+//    Console.WriteLine(item.ToString());
+//}
+Console.ReadLine();
---
+++ 02. C# And .NET/09. Internals/DotNetInternal/DotNetInternal.RefAndOut/Program.cs
@@ -0,0 +1,35 @@
+﻿int myVariable = 10;
+int myVariableOut;
+AddNumberByRef(ref myVariable);
+AddNumberByRefWithOut(out myVariableOut);
+Console.WriteLine(myVariable);
+Console.WriteLine(myVariableOut);
+Console.WriteLine("---------------------------");
+List<int> ints = [1, 2, 3, 4];
+AddToList(ref ints);
+foreach (int i in ints)
+{
+    Console.WriteLine(i);
+}
+Console.ReadLine();
+
+
+
+void AddNumberByRef(ref int number)
+{
+
+}
+
+bool AddNumberByRefWithOut(out int number)
+{
+    number = 123;
+    return true;
+}
+
+void AddToList(ref List<int> list)
+{
+    //list.Add(DateTime.Now.Day);
+    //list.Add(DateTime.Now.Second);
+    //list.Add(DateTime.Now.Millisecond);
+    list = null;
+}
