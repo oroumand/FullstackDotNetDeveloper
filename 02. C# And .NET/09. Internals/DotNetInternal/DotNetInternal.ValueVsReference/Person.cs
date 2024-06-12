b7c2cb25e9e4c87d3c3655fc465a65830e051298
oroumand <ar.oroumand@gmail.com>
2024-06-12 09:52:31 +0330

    DotNetInternal.ValueVsReference

---
+++ 02. C# And .NET/09. Internals/DotNetInternal/DotNetInternal.ValueVsReference/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetInternal.ValueVsReference
+{
+    public struct Person
+    {
+        public int Age { get; set; }
+    }
+}
---
+++ 02. C# And .NET/09. Internals/DotNetInternal/DotNetInternal.ValueVsReference/Program.cs
@@ -0,0 +1,29 @@
+﻿using DotNetInternal.ValueVsReference;
+
+int myAge = 40;
+int yourAge = myAge;
+Person me = new Person();
+me.Age = 40;
+Person you = me;
+you.Age++;
+
+addAge(myAge);
+AddPersonAge(me);
+yourAge++;
+Console.WriteLine($"My age is= {myAge}");
+Console.WriteLine($"Your age is= {yourAge}");
+
+Console.WriteLine($"My age in class is= {me.Age}");
+Console.WriteLine($"Your age in class is= {you.Age}");
+Console.ReadLine();
+
+
+void addAge(int age)
+{
+    age++;
+}
+
+void AddPersonAge(Person person)
+{
+    person.Age++;
+}
